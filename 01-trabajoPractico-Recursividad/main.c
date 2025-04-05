082bd19c9a3f233054762cb8f7a5cbb99c728bfd
Augusto Rolandelli <84287366+chirogod@users.noreply.github.com>
2025-04-05 18:48:57 -0300

    update
    menu y ejercicios 1, 2, 3, 4 y 7

--- 01-trabajoPractico-Recursividad/main.c
+++ 01-trabajoPractico-Recursividad/main.c
@@ -5,18 +5,37 @@
 int main(){
     int ejercicio;
     while(1){
-        printf("Ingrese el ejercicio al que ingresar: ");
+        printf("----MENU----\n");
+        printf("1)Palindromo\n");
+        printf("2)Producto mediante sumas sucesivas\n");
+        printf("3)Fibonacci\n");
+        printf("4)Division mediante restas sucesivas\n");
+        printf("7)Onda digital\n");
+        printf("Ingrese el ejercicio al que ingresar('0' PARA SALIR): ");
         scanf("%d", &ejercicio);
-        if(ejercicio == 1){
-            ejercicio1();
-        }else if(ejercicio==2){
-            ejercicio2();
-        }else if(ejercicio==3){
-            ejercicio3();
-        }else if(ejercicio==4){
-            ejercicio4();
-        }else if(ejercicio==7){
-            ejercicio7();
+        switch(ejercicio){
+            case 0:
+                printf("\nSaliendo del programa!!!\n");
+                return 0;
+            case 1:
+                ejercicio1();
+                break;
+            case 2:
+                ejercicio2();
+                break;
+            case 3:
+                ejercicio3();
+                break;
+            case 4:
+                ejercicio4();
+                break;
+            case 7:
+                ejercicio7();
+                break;
+            default:
+                printf("Ese ejercicio no existe, solo del 1 al 10.");
+
         }
+
     }
 }
